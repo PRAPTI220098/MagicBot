390d07fbfaa8f0012c464ddebb629fba331c8ba4
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-17 21:58:22 +0530

    Update hh.py

--- hh.py
+++ hh.py
@@ -8,7 +8,7 @@ def home():
     return "<b>hello</b>"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=7861)
 
 def keep_alive():
     t = threading.Thread(target=run)
